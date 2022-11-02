46f062de0379493dffb121fd1e4234bae626601f
Andrea Romero <andrearx52@gmail.com>
2022-11-02 20:00:54 -0300

    Changing color

--- my-drinksmarket/src/App.js
+++ my-drinksmarket/src/App.js
@@ -2,11 +2,13 @@ import NavBar from './components/NavBar/NavBar';
 import ItemsListContainer  from './components/ItemsListContainer/ItemsListContainer';
 import './App.css';
 
+
 function App() {
   return (
     <>
     <NavBar/>
     <ItemsListContainer name="Bienvenidos a Monkey Drinks, eres mayor de edad?"/>
+
     </>
 );
  
--- my-drinksmarket/src/components/ItemsListContainer/ItemsListContainer.js
+++ my-drinksmarket/src/components/ItemsListContainer/ItemsListContainer.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './ItemsListContainer.css'
 
+
+
 const ItemsListContainer = (props) => {
-  return (
-    <div className='message-greeting-container'>
+  
+    return (
+    <div className='greeting-container'>
         <h2 className='message'>{props.name}</h2>
 
     </div>
